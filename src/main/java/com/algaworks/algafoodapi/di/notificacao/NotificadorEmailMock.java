ddcbb2ac76e888c313fb6b07f48bf631c45e1b11
Leandro D.B <leandro@unemix.com.br>
2022-11-22 18:24:14 -0300

    Aula 2.20 Profiles

--- src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmailMock.java
@@ -1,31 +1,20 @@
 package com.algaworks.algafoodapi.di.notificacao;
 
 import com.algaworks.algafoodapi.di.modelo.Cliente;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
+@Profile("dev")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
-public class NotificadorEmail implements Notificador {
+public class NotificadorEmailMock implements Notificador {
 
-    public NotificadorEmail() {
-        System.out.println("NotificadorEmail");
-
+    public NotificadorEmailMock() {
+        System.out.println("NotificadorEmail Mock");
     }
-
-
-
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-
-        System.out.printf("Notificando %s através do e-mail %s: %s",
+        System.out.printf("Simulando e-mail via Mock %s através do e-mail %s: %s",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
-
-
-
 }
--- src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AtivacaoClienteService {
 
-    @TipoDoNotificador(NivelUrgencia.URGENCIA)
+    @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
     @Autowired
     private Notificador notificador;
 
