3483d5bb5a25be8b4fd548ae484c474b52aa4f7b
Leandro D.B <leandro@unemix.com.br>
2022-11-24 20:15:32 -0300

    Aula 3.6 a 3.7

---
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafoodapi.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table
+public class Restaurante {
+
+
+    @Id
+    private int Id;
+
+    private String nome;
+
+    @Column(name="taxa_entrega")
+    private BigDecimal taxaEntrega;
+
+}
