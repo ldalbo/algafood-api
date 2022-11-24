64dcf491f36eaee5542ff90eaf405ba3afee4985
Leandro D.B <leandro@unemix.com.br>
2022-11-24 11:07:59 -0300

    Aula 3.3 tirando pacotes e classe para coemcar o Modulo 3

--- src/main/java/com/algaworks/algafoodapi/di/notificacao/NivelUrgencia.java
+++
@@ -1,5 +0,0 @@
-package com.algaworks.algafoodapi.di.notificacao;
-
-public enum NivelUrgencia {
-    SEM_URGENCIA, URGENCIA
-}
---
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Cozinha.java
@@ -0,0 +1,2 @@
+package com.algaworks.algafoodapi.domain.model;public class Cozinha {
+}
