fdab07bafc9836911bcaef71e19934ce1a7d4150
Leandro D.B <leandro@unemix.com.br>
2022-12-01 16:14:38 -0300

    Introduzido o ResponseEntity

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,26 +23,41 @@ public class CozinhaController {
     CozinhaRepository cozinhaRepository;
 
     // ACRESCENTA NO FINAL DA PRINCIPAL, NESSE CASO NADA
-    /@GetMapping
-    // @GetMapping(path = "")
-    public List<Cozinha> listar(){
-        return cozinhaRepository.todas();
+
+    @GetMapping
+    public ResponseEntity<List<Cozinha>> listar(){
+       List<Cozinha> cozinhas =  cozinhaRepository.todas();
+       return ResponseEntity.ok(cozinhas);
+   }
+
+    /*
+    @GetMapping
+    public ResponseEntity<String> teste(){
+
+        String a = "23";
+        return ResponseEntity.ok(a);
     }
-
-
+    */
 
     /* PRIMEIRA  FORMA EXPLICITANDO
     @GetMapping("/{cozinhaId}") //AQUI PODE SER QUALQUER NOME
-    public Cozinha buscar(@PathVariable("cozinhaId") Long id){
+    public Cozinha buscar(@PathVariable("cozinhaId") Long id){ // AQUI CASO COM O ATRIBUTO QUE VEM DE CIMA
         return  cozinhaRepository.porId(id);
     }
 
      */
 
-    @GetMapping("{cozinhaId}") //AQUI PODE SER QUALQUER NOME
-    public Cozinha buscar(@PathVariable Long cozinhaId){
-        return  cozinhaRepository.porId(cozinhaId);
+   @GetMapping("{cozinhaId}") //AQUI PODE SER QUALQUER NOME
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId){
+        Cozinha cozinha =  cozinhaRepository.porId(cozinhaId);
+
+        // return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+        return  ResponseEntity.ok(cozinha);
+
     }
+
+
+
 
 
 
