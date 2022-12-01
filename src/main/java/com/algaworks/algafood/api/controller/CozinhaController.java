2d0ed0cedca1f0ddced2474809e4dc2a05a111db
Leandro D.B <leandro@unemix.com.br>
2022-12-01 15:17:13 -0300

    Fazendo experiencias com @GetMapping

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,6 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,19 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+
 @RestController
+// AQUI FICA O DIRETÓRIO RAIZ O QUE VIRIA DEPOIS DO "localhost:8080"
 @RequestMapping("/cozinhas")
 public class CozinhaController {
 
     @Autowired
     CozinhaRepository cozinhaRepository;
 
-     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE )
-
-    // @GetMapping("/cozinhas")
+    // ACRESCENTA NO FINAL DA PRINCIPAL, NESSE CASO NADA
+    /@GetMapping
+    // @GetMapping(path = "")
     public List<Cozinha> listar(){
         return cozinhaRepository.todas();
     }
+
+
+
     /* PRIMEIRA  FORMA EXPLICITANDO
     @GetMapping("/{cozinhaId}") //AQUI PODE SER QUALQUER NOME
     public Cozinha buscar(@PathVariable("cozinhaId") Long id){
@@ -32,7 +37,7 @@ public class CozinhaController {
 
      */
 
-    @GetMapping("/{cozinhaId}") //AQUI PODE SER QUALQUER NOME
+    @GetMapping("{cozinhaId}") //AQUI PODE SER QUALQUER NOME
     public Cozinha buscar(@PathVariable Long cozinhaId){
         return  cozinhaRepository.porId(cozinhaId);
     }
