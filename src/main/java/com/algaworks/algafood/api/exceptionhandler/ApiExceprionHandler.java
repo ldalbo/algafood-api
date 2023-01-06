06ab3d0c5ee0562daafcd30f5ef9a5643ac3e5f1
Leandro D.B <leandro@unemix.com.br>
2023-01-06 15:32:25 -0300

    Aula 08.22 tratamento de formato

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceprionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceprionHandler.java
@@ -3,6 +3,9 @@ package com.algaworks.algafood.api.exceptionhandler;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,9 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ApiExceprionHandler extends ResponseEntityExceptionHandler {
@@ -20,14 +26,35 @@ public class ApiExceprionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Throwable rootCause = ExceptionUtils.getRootCause(ex);
+
+        if (rootCause instanceof InvalidFormatException){
+            return handleInvalidFormatException( (InvalidFormatException) rootCause, headers,  status,  request);
+        }
 
         ProblemType problemType = ProblemType.JSON_INVALIDO;
-
         String detail = "Formato inválido no JSON";
         Problem problem = createProblemBuilder(status,problemType, detail).build();
-
         return handleExceptionInternal(ex,problem,new HttpHeaders(),status,request);
     }
+
+    private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException ex,
+                                                                HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        String path = ex.getPath().stream()
+                .map(ref -> ref.getFieldName())
+                .collect(Collectors.joining("."));
+
+        ProblemType problemType = ProblemType.JSON_INVALIDO;
+        String detail = String.format("A propriedade '%s' recebeu o valor '%s', "
+                        + "que é de um tipo inválido. Corrija e informe um valor compativel com o tipo %s.",
+                path, ex.getValue(), ex.getTargetType().getSimpleName());
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
+    }
+
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException e, WebRequest request){
