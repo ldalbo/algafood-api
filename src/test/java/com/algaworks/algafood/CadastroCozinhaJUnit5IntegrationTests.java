5e20ea04a39b203b003508acf3a446d6aafac350
Leandro D.B <leandro@unemix.com.br>
2023-01-24 15:36:41 -0300

    Aula 10.05 testes Locais OK

--- src/test/java/com/algaworks/algafood/CadastroCozinhaJUnit5IntegrationTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaJUnit5IntegrationTests.java
@@ -7,10 +7,8 @@ import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.validation.ConstraintViolationException;
 
@@ -18,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 // @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class CadastroCozinhaIntegrationJUnit5Tests {
+public class CadastroCozinhaJUnit5IntegrationTests {
 
     @Autowired
     private CadastroCozinhaService cadastroCozinha;
