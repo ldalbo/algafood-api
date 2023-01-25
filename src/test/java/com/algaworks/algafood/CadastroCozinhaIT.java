2ac3d628cf5877d38dbc500e2fdbe707783fea55
Leandro D.B <leandro@unemix.com.br>
2023-01-25 09:27:04 -0300

    Aula 10.08 com problema de travamento

---
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood;
+
+
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import io.restassured.http.ContentType;
+import org.springframework.http.HttpStatus;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CadastroCozinhaIT {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        System.out.println("Porta: " + port);
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+        .when()
+                .get()
+        .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
