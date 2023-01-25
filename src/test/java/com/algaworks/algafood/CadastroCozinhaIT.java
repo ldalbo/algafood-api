16c6a6f66ded7dcad45dd59fafa04b969ce39f7a
Leandro D.B <leandro@unemix.com.br>
2023-01-25 19:59:03 -0300

    Aula 10.08 toda bugada

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -3,16 +3,18 @@ package com.algaworks.algafood;
 
 
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import io.restassured.RestAssured;
-import static io.restassured.RestAssured.given;
-import io.restassured.http.ContentType;
-import org.springframework.http.HttpStatus;
+
+import static io.restassured.RestAssured.when;
+
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -21,6 +23,27 @@ public class CadastroCozinhaIT {
     @LocalServerPort
     private int port;
 
+    @BeforeEach
+    public void setup() {
+        RestAssured.port = port;
+    }
+    /*
+    @Test
+    public void whenLogResponse_thenOK() {
+        when()
+                .get("/cozinhas")
+                .then().log().body().statusCode(200);
+    }
+    */
+
+    @Test
+    public void whenLogResponse_thenOK2() {
+        var a = RestAssured.when()
+                .get("/cozinhas")
+                .then().log().body().statusCode(200);
+        System.out.println(a);
+    }
+    /*
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
@@ -34,4 +57,23 @@ public class CadastroCozinhaIT {
         .then()
                 .statusCode(HttpStatus.OK.value());
     }
+    */
+
+/*
+    @Test
+    public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+                // .when()
+               // .get()
+                .then()
+                .body("", hasSize(4));
+//			.body("nome", hasItems("Indiana", "Tailandesa"));
+    }
+
+ */
 }
