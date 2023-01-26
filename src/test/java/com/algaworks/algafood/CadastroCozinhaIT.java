f63a9e1fa59b9e25436e30df78f2a674f87509fb
Leandro D.B <leandro@unemix.com.br>
2023-01-25 21:21:59 -0300

    Aula 10.08 voltando ao ponto enviado para Algaworks 25.01.23

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -3,18 +3,16 @@ package com.algaworks.algafood;
 
 
 
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
-import static io.restassured.RestAssured.when;
-
-
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import io.restassured.http.ContentType;
+import org.springframework.http.HttpStatus;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -23,27 +21,6 @@ public class CadastroCozinhaIT {
     @LocalServerPort
     private int port;
 
-    @BeforeEach
-    public void setup() {
-        RestAssured.port = port;
-    }
-    /*
-    @Test
-    public void whenLogResponse_thenOK() {
-        when()
-                .get("/cozinhas")
-                .then().log().body().statusCode(200);
-    }
-    */
-
-    @Test
-    public void whenLogResponse_thenOK2() {
-        var a = RestAssured.when()
-                .get("/cozinhas")
-                .then().log().body().statusCode(200);
-        System.out.println(a);
-    }
-    /*
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
@@ -57,23 +34,4 @@ public class CadastroCozinhaIT {
         .then()
                 .statusCode(HttpStatus.OK.value());
     }
-    */
-
-/*
-    @Test
-    public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-
-        given()
-                .basePath("/cozinhas")
-                .port(port)
-                .accept(ContentType.JSON)
-                // .when()
-               // .get()
-                .then()
-                .body("", hasSize(4));
-//			.body("nome", hasItems("Indiana", "Tailandesa"));
-    }
-
- */
 }
