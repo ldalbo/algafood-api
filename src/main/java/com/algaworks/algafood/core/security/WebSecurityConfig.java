58c640809f0bc2f67c4168e628f8333066ec3af4
Leandro D.B <leandro@unemix.com.br>
2023-08-14 14:32:23 -0300

    Aula 22.4 usuários em memória

--- src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -4,25 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.WebMvcSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig  {
+@EnableWebSecurity
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter { // Cuidar pois está depreciado
 
-
-    /*
-    @Bean
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
-                .withUser("thiago")
-                .password("123")
-                .roles("ADMIN"); // Obrigatório, mas não serve para nada
-    }
-    */
-
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http)  throws Exception {
         http
                 .httpBasic()
                 .and()
@@ -38,8 +33,31 @@ public class WebSecurityConfig  {
                 .and()
                     .csrf().disable(); // Assim libera os métodos post
 
-        return http.build();
+       // return http.build();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        System.out.println(passwordEncoder().encode("123"));
+
+        auth.inMemoryAuthentication()
+                .withUser("joao")
+                    .password(passwordEncoder().encode("123"))
+                    .roles("ADMIN")
+                .and()
+                   .withUser("maria")
+                 .password(passwordEncoder().encode("123"))
+                .roles();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+
 }
 
 
